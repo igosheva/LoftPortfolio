779217ea2783105a1a8c6bd5162073476c351857
Nastasia <just_anasteziya@mail.ru>
2017-02-15 18:07:28 +0300

    add fonts

--- gulp/tasks/sprite.png.js
+++ gulp/tasks/sprite.png.js
@@ -14,7 +14,7 @@ module.exports = function () {
       .pipe($.gulp.dest($.config.root + '/assets/images'));
 
     var cssStream = spriteData.css
-      .pipe($.gulp.dest('./source/style/common'));
+      .pipe($.gulp.dest('./source/style/helpers'));
 
     return $.merge(imgStream, cssStream);
   });
